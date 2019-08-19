52dcbc1ec5639039f856d57f927c2c950bdd5486
Miles.Hamilton <46730797+MilesHamilton@users.noreply.github.com>
2019-08-19 15:32:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var todoList = {
     todos: [],
     displayTodos: function() {
-        console.log('My Todos', this.todos);
-    },
+        if (this.todos.length === 0) {
+        console.log('your todo list is empty!!')
+        } else {
+            console.log('My Todos:');
+        for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+            console.log ('(x)', this.todos[i].todoText)
+        } else {
+            console.log ('( )', this.todos[i].todoText)
+            }    
+        }
+    }
+},
     addTodo: function(todoText) {
         this.todos.push({
             todoText: todoText,
@@ -21,9 +32,30 @@ var todoList = {
         this.todos.splice(position,1);
         this.displayTodos();
     },
-    toggleCompleted (position) {
+    toggleCompleted: function(position) {
         var todo = this.todos [position];
         todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length
+        var completedTodos = 0;
+
+    for (var i = 0; i < totalTodos.length; i++) {
+        if(this.todos[i].completed === true) {
+            completedTodos++;
+        }
+    }
+        if (completedTodos === totalTodos) {
+            for (var i =0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for (let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+                
+            }
+        }
         this.displayTodos();
     }
 };
